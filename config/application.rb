ff1280cbff97611593b79bd23ebeb2dd851d134c
Shane Rogers <rogerssh@tcd.ie>
2015-07-22 09:01:55 -0700

    Add active record callback non-swallow exceptions

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module MaykServer
                  methods: [:get, :put, :post, :patch, :delete, :options]
       end
     end
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
